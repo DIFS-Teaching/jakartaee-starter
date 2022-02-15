04745ca5fa97e9f451d5716d544512b015ad3648
Radek Burget <burgetr@fit.vutbr.cz>
2022-02-15 20:57:05 +0100

    Backport to Jakarta 8

--- src/main/java/cz/vut/fit/pis/start/servlet/HelloServlet.java
+++ src/main/java/cz/vut/fit/pis/start/servlet/HelloServlet.java
@@ -2,11 +2,12 @@ package cz.vut.fit.pis.start.servlet;
 
 import java.io.IOException;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 
 @WebServlet(urlPatterns="/hello")
 public class HelloServlet extends HttpServlet {
